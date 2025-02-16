b21053174b4246115de16f7c115ea6220aecab1c
Ghazanfar Mahmood <ghazanfarmahmood14@gmail.com>
2025-02-16 22:27:03 +0500

    NavBar position fixed

--- src/component/navbar/NavBar.jsx
+++ src/component/navbar/NavBar.jsx
@@ -13,19 +13,10 @@ import React from 'react';
 import Image from 'next/image';
 
 
-//    const changeNavBar = () => {
-//         if (window.scrollY >= 210) {
-//             setBackground(true)
-//         } else {
-//             setBackground(false)
-//         }
-//     }
-//     window.addEventListener("scroll", changeNavBar);
-
-
 export default function NavBar() {
     const [className, setClassName] = useState("");
     const [background, setBackground] = useState(false);
+    const [fixedNav, setFixedNav] = useState(false)
 
     const openMenu = () =>{
         setClassName("block");
@@ -36,45 +27,61 @@ export default function NavBar() {
         setBackground(false);
     }
     
+    useEffect(() =>{
+        const changeNavBar = () => {
+         if (window.scrollY >= 210) {
+            setFixedNav(true)
+         } else {
+            setFixedNav(false)
+         }
+     }
+     window.addEventListener("scroll", changeNavBar);
+
+     return () => {
+        window.removeEventListener("scroll", changeNavBar);
+    };
+    }, [])
 
     return (
         <>
-        <div className="nav-container">
-            <a href="#">
-                <div className="logo">
-                    <Image src="/assets/logo.jpg" alt="ERROR" width="100" height="42" />
-                </div>
-            </a>
-            <div className={`menu ${className}`}>
-                <div className='close'>
-                    <button aria-label='close button' onClick={closeMenu}>
-                        <MdClose />
-                    </button>
-                </div>
-                <div className="web-menu">
-                    <div className='left'>
-                        <a href="#home">Home</a>
-                        <a href="#about">About Us</a>
-                        <a href="#services">Services</a>
-                        <a href="#gallery">Gallery</a>
-                        <a href="#review">Reviews</a>
-                        <a href="#contact">Contact Us</a>
+        <div className={`${fixedNav ? "fixed-nav" : ""}`}>
+            <div className="nav-container">
+                <a href="#">
+                    <div className="logo">
+                        <Image src="/assets/logo.jpg" alt="ERROR" width="100" height="42" />
+                    </div>
+                </a>
+                <div className={`menu ${className}`}>
+                    <div className='close'>
+                        <button aria-label='close button' onClick={closeMenu}>
+                            <MdClose />
+                        </button>
+                    </div>
+                    <div className="web-menu">
+                        <div className='left'>
+                            <a href="#home">Home</a>
+                            <a href="#about">About Us</a>
+                            <a href="#services">Services</a>
+                            <a href="#gallery">Gallery</a>
+                            <a href="#review">Reviews</a>
+                            <a href="#contact">Contact Us</a>
+                        </div>
+                    </div>
+                    <div className='right'>
+                        <a href="#">Contact Us</a>
                     </div>
                 </div>
-                <div className='right'>
-                    <a href="#">Contact Us</a>
+                <div className='hamburger'>
+                    <button aria-label='for menu open' onClick={openMenu}>
+                        <IoMenu />
+                    </button>
                 </div>
             </div>
-            <div className='hamburger'>
-                <button aria-label='for menu open' onClick={openMenu}>
-                    <IoMenu />
-                </button>
-            </div>
         </div>
-        {
-            background &&
-            <div className='black-bg' onClick={closeMenu}></div>
-        }
-    </>
+            {
+                background &&
+                <div className='black-bg' onClick={closeMenu}></div>
+            }
+        </>
     )
 }
